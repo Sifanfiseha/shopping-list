0bce7620c027a330b007aaa3d523db83b7d51406
sifanfiseha <sifen.fiseha@astu.edu.et>
2025-01-28 21:38:52 -0500

    implemented an add item functionality

---
+++ script.js
@@ -0,0 +1,34 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+
+function addItem(e) {
+  e.preventDefault();
+  const newItem = itemInput.value;
+  if (newItem === "") {
+    alert("please add an item!");
+    return;
+  }
+  // create a new list element
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  console.log(li);
+  const button = createButton("remove-item btn-link text-red");
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  li.appendChild(button);
+  itemList.append(li);
+  itemInput.value = "";
+}
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  return button;
+}
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+// EventListeners
+itemForm.addEventListener("submit", addItem);
